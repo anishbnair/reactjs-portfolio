5537c7e558a5b1b2bfaf29b1a5b387e3a2206b45
Anish Balachandran Nair <anishbnair@Anishs-MacBook-Pro.local>
2021-04-14 22:07:24 -0400

    updated files

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@
 //import './App.css';
 
 import './App.scss';
+import Navbar from './Components/NavBar';
 //import './Styles.scss';
+import NavBar from './Components/NavBar'
 
 function App() {
   return (
     <div className="App">
       <div className="sidebar"></div>
+        <Navbar />
       <div className="main-content"></div>
     </div>
   );
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,13 +1,49 @@
 import React from 'react'
-import avatar from './'
+import avatar from '../img/avatar.png'
+import {NavLink} from 'react-router-dom'
 
 function Navbar() {
     return (
         <div className="NavBar">
-            <div className="profile">
-                <img src={avatar} alt=""/>
 
-            </div>
+            <nav className="nav">
+                <div className="profile">
+                    <img src={avatar} alt=""/>
+                </div>
+                <ul className="nav-items">
+                    <li className="nav-item">
+                        <NavLink to="/home" exact activeClassName="active">
+                            Home
+                        </NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink to="/about" exact activeClassName="active">
+                            About
+                        </NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink to="/portfolios" exact activeClassName="active">
+                            Portfolios
+                        </NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink to="/blogs" exact activeClassName="active">
+                            Blogs
+                        </NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink to="/contact" exact activeClassName="active">
+                            Contact
+                        </NavLink>
+                    </li>
+                </ul>
+                <footer className="footer">
+                    <p>
+                        @2021 Lorem Ipsum
+                    </p>
+
+                </footer>
+            </nav>
 
         </div>
     )
